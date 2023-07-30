acf4d6f958b7b65c21a07e1a05f65810dbbd7527
Arjit <arjitsrivastava13@gmail.com>
2023-07-30 20:13:54 +0200

    Add support for Apple Status Bar Style

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
           <link
             href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
             rel="stylesheet"
